10da507db97714bb4ab11946e7d38416a1aba191
Aditya Pathak <68177924+Phoenix1504e@users.noreply.github.com>
2026-08-10 17:42:27 +0000

    chore(typing): add stubs for grpc_gen to clear attr-defined errors

--- src/bernstein/core/grpc_gen/__init__.py
+++ src/bernstein/core/grpc_gen/__init__.py
@@ -1,1 +1,9 @@
 """Generated gRPC stubs - run ``scripts/generate_proto.sh`` to populate."""
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from . import cluster_pb2 as cluster_pb2
+    from . import cluster_pb2_grpc as cluster_pb2_grpc
+    from . import tasks_pb2 as tasks_pb2
+    from . import tasks_pb2_grpc as tasks_pb2_grpc
